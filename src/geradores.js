1ac3d4b867fb60da9fc3a6429349d8e24017598e
Breno Pavanelli dos Santos <138505121+brenopavanelli@users.noreply.github.com>
2025-09-29 16:51:26 +0000

    feat: function gerarSenha implementada

--- src/geradores.js
+++ src/geradores.js
@@ -5,3 +5,17 @@ const geraNumero = () => String.fromCharCode(rand(48, 57));
 const simbolos = ',.;^~[]{}!@#$%*()_+=-';
 const geraSimbolo = () => simbolos[rand(0, simbolos.length - 1)];
 
+export default function gerarSenha(qtd, maiusculas, minusculas, numeros, simbolos) {
+    const senhaArray = [];
+    qtd = Number(qtd);
+    for (let i = 0; i < qtd; i++) {
+      maiusculas && senhaArray.push(geraMaiuscula());
+      minusculas && senhaArray.push(geraMinuscula());
+      numeros && senhaArray.push(geraNumero());
+      simbolos && senhaArray.push(geraSimbolo());
+    }
+    const arrayAleatorio = senhaArray.sort(() => Math.random() - 0.5);
+    const senha = arrayAleatorio.join('').slice(0, qtd);
+  
+    return senha;
+}
